ef19c1d0c1594492f30b71780b87e31ba061f4a8
¨Oseas¨ <=>
2022-11-08 10:29:22 -0600

    Agregado supabase

--- src/componentes/Registro.jsx
+++ src/componentes/Registro.jsx
@@ -3,34 +3,34 @@ import { FormControl, FormLabel, Grid, Heading, Input, Stack, Button } from '@ch
 const Registro = () => {
     return (
         <>
-            <Heading fontSize='2xl'>Registro</Heading>
+            <Heading fontSize='2xl' mb='20px'>Registro</Heading>
             <form>
                 <Grid templateColumns='repeat(2, 1fr)' gap={4}>
                     <FormControl id='nombre' isRequired>
                         <FormLabel>Nombre</FormLabel>
-                        <Input type='texto' name='nombre' placeHolder='nombre' />
+                        <Input type='texto' name='nombre' placeHolder='Nombre'/>
                     </FormControl>
                     <FormControl id='apellido' isRequired>
                         <FormLabel>Apellido</FormLabel>
-                        <Input type='texto' name='apellido' placeHolder='Apellido' />
+                        <Input type='texto' name='apellido' placeHolder='Apellido'/>
                     </FormControl>
                 </Grid>
                 <Stack spacing={4}>
                     <FormControl id='correo' isRequired>
                         <FormLabel>Correo</FormLabel>
-                        <Input type='email' name='correo' placeHolder='Correo' />
+                        <Input type='email' name='correo' placeHolder='Correo'/>
                     </FormControl>
                     <FormControl id='contraseña' isRequired>
                         <FormLabel>Contraseña</FormLabel>
-                        <Input type='password' name='contraseña' placeHolder='contraseña' />
+                        <Input type='password' name='contraseña' placeHolder='contraseña'/>
                     </FormControl>
                     <FormControl id='confirmarContraseña' isRequired>
                         <FormLabel>Confirmar Contraseña</FormLabel>
-                        <Input type='password' name='confirmarContraseña' placeHolder='Confirmar Contraseña' />
+                        <Input type='password' name='confirmarContraseña' placeHolder='Confirmar Contraseña'/>
                     </FormControl>
                     <FormControl id='telefono' isRequired>
                         <FormLabel>Telefono</FormLabel>
-                        <Input type='texto' name='telefono' placeHolder='telefono' />
+                        <Input type='texto' name='telefono' placeHolder='telefono'/>
                     </FormControl>
                     <Button colorScheme='blue'>Registrar</Button>
                 </Stack>
---
+++ src/servicios/auth.js
@@ -0,0 +1,3 @@
+ import { supabase } from "../api/config";
+
+ console.log(supabase);
