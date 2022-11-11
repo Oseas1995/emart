f0a9bb4c8e37dc11392562d377270d9153124567
Carlos1098 <54784427+Carlos1098@users.noreply.github.com>
2022-11-10 19:02:21 -0600

    Modal1
    Muestra del primer modal

---
+++ src/componentes/Modal.jsx
@@ -0,0 +1,39 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react'
+import "./Modal.css"
+
+function VentanaModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <div>
+      <button class="button" onClick={onOpen}>Abrir modal</button>
+      <Modal isOpen={isOpen} onClose ={onClose}>
+        <ModalOverlay>
+            <ModalContent>
+              <ModalHeader>Politicas </ModalHeader>
+              <ModalCloseButton></ModalCloseButton>
+              <ModalBody>
+                <lorem count={2}></lorem>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe, velit asperiores tenetur, corporis rerum voluptatum facere quidem inventore, sequi et enim deleniti neque dolorem mollitia minus perferendis atque! Optio, voluptatem!</p>
+              </ModalBody>
+              <ModalFooter>
+                <button colorScheme='blue' mr={3} onClick={onclose}>
+                  Aceptar
+                </button>
+                <button variant='guost'  >No aceptar</button>
+              </ModalFooter>
+            </ModalContent>
+        </ModalOverlay>
+      </Modal>
+    </div>
+  )
+}
+export default VentanaModal
--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Home from "../componentes/Home"
 import Login from "../componentes/Login"
 import Registro from "../componentes/Registro"
+import VentanaModal from "../componentes/Modal"
 import "../index.css"
 const AppRouter = () => {
 
@@ -11,6 +12,7 @@ const AppRouter = () => {
             <Route path="/home" element={<Home />} />
             <Route path="/registro" element={<Registro />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/modal" element={<VentanaModal/>} />
         </Routes>
     )
 }
