99ae1c679d64ea44b09cb9ec15f76a0f3163d93f
JeisonAguilar <geovacalix@gmail.com>
2022-11-11 23:38:30 -0600

    header actualizado

--- src/componentes/Cabecera.jsx
+++ src/componentes/Cabecera.jsx
@@ -1,9 +1,48 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Cabecera.css"
-function Cabecera(){
-    return(
-        <div className="cabecera">
-            <h1>Esta es la cabecera</h1>
+import { HamburgerIcon, Search2Icon } from '@chakra-ui/icons'
+
+
+
+function Cabecera() {
+    return (
+        <div className='header'>
+            <div className='header_menuicon'>
+                <HamburgerIcon w={10} h={10} className='header_icon' />
+
+            </div>
+
+
+            <div className='header_text'> 
+                    <Link to='/home'>
+
+                        PAGINA PRINCIPAL
+
+                    </Link>
+            </div>
+            <div className='header_searchbar'>
+                <input className='header_bar' />
+                <Search2Icon w={30} h={30} className='header_searchicon' />
+
+            </div>
+
+            <div className='header_text'>
+                <Link to='/Login'>
+
+                        INICIAR SESION
+                        
+                    </Link>
+
+            </div>
+
+
+
+
+
+
         </div>
+
     )
 }
 
