c3c3ff8456a4d837323b89daa8888ecb2756534a
Fernando0Murillo <86390468+Fernando0Murillo@users.noreply.github.com>
2022-11-12 17:06:13 -0600

    agredo

--- src/componentes/Cabecera.jsx
+++ src/componentes/Cabecera.jsx
@@ -1,29 +1,49 @@
 import "./Cabecera.css"
+import { Link } from 'react-router-dom'
 import { React } from 'react';
-import { Image, Flex, Button, HStack, Text, chakra } from '@chakra-ui/react';
+import { HamburgerIcon, Search2Icon } from '@chakra-ui/icons'
+
 
 
 function Cabecera() {
-
     return (
-        <chakra.header
-            bg="black"
-        >
-            <Flex
-                w="100%"
-                px = "6"
-                py = "5"
-                align = "center"
-                justify= "space-between"
-            >
-                <Text 
-                    color="white" 
-                    as='h1'
-                    fontSize="24px"
-                >Cabecera</Text>
-            </Flex>
-        </chakra.header>
-    );
+        <div className='header'>
+            <div className='header_menuicon'>
+                <HamburgerIcon w={10} h={10} className='header_icon' />
+
+            </div>
+
+
+            <div className='header_text'> 
+                    <Link to='/home'>
+
+                        PAGINA PRINCIPAL
+
+                    </Link>
+            </div>
+            <div className='header_searchbar'>
+                <input className='header_bar' />
+                <Search2Icon w={30} h={30} className='header_searchicon' />
+
+            </div>
+
+            <div className='header_text'>
+                <Link to='/Login'>
+
+                        INICIAR SESION
+
+                    </Link>
+
+            </div>
+
+
+
+
+
+
+        </div>
+
+    )
 }
 
 export default Cabecera;
